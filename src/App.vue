0419a09e345c6af25c2bbbc208717795f9392e3b
Geridev <lovasgergo44@gmail.com>
2022-03-15 22:01:27 +0100

    nem

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script>
+import { reactive, onMounted, ref } from 'vue';
+import ws from './ws';
+
+export default {
+  setup() {
+    const inputUsername = ref("");
+    const inputMessage = ref("");
+
+    const state = reactive({
+      username: "",
+      message: [],
+    });
+    const Login = () => {
+      if (inputUsername.value != "" || inputUsername.value != null) {
+        state.username = inputUsername.value;
+        ws.send(state.username);
+        inputUsername.value = "";
+      }
+    }
+    const MessageSend = () => {
+      if (inputMessage.value == "" || inputMessage.value == null) {
+        return;
+      }
+      ws.send(JSON.stringify(inputMessage.value));
+      inputMessage.value = "";
+    }
+
+    onMounted(() => {
+      ws.onmessage = (msg) => {
+        let obj = JSON.parse(msg.data);
+        state.message.push({
+          rmsg: obj.msg,
+          name: obj.UserName.name,
+        })
+      }
+    })
+    return {
+      inputUsername,
+      state,
+      Login,
+      inputMessage,
+      MessageSend,
+    }
+  }
+}
+  
+</script>
+
+<template>
+  <div v-if="state.username === '' || state.username === null">
+    <form @submit.prevent="Login">
+      <input type="text" v-model="inputUsername" placeholder="username">
+      <input type="submit" value="Send">
+    </form>
+  </div>
+  <div v-else>
+    <section>
+      <div v-for="msg in state.message" :key="msg.key">
+        <div><b>{{ msg.name }}:</b>{{ msg.rmsg }}</div>
+      </div>
+    </section>
+    <footer>
+      <form @submit.prevent="MessageSend">
+        <input type="text" v-model="inputMessage" placeholder="message">
+        <input type="submit" value="Send">
+      </form>
+    </footer>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/ws.js
@@ -0,0 +1,3 @@
+const socket = new WebSocket("ws://164.92.239.45:8080/ws");
+
+export default socket
